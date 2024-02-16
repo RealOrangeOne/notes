c988131d09fe490103bfc2822d8411e22479fb0d
Jake Howard <git@theorangeone.net>
2024-02-16 16:13:07 +0000

    Add notes list and optimise getting notes

---
+++ docs/notes/index.md
@@ -0,0 +1,6 @@
+---
+template: all.html
+hide: [toc]
+---
+
+# Notes
--- hooks/notes.py
+++ hooks/notes.py
@@ -1,2 +1,30 @@
+import jinja2
+
+@jinja2.pass_context
+def get_page(context, slug):
+    nav = context["nav"]
+    for page in nav.pages:
+        if page.file.src_uri == slug:
+            return page
+
+    raise ValueError("Unable to find page for '%s'", slug)
+
+
+@jinja2.pass_context
+def get_notes(context):
+    notes = []
+    for page in context["nav"].pages:
+        if not page.file.src_uri.startswith("notes/"):
+            continue
+
+        if page.file.src_uri == "notes/index.md":
+            continue
+
+        notes.append(page)
+    return sorted(notes, key=lambda p: p.meta["git_creation_date_localized_raw_iso_date"], reverse=True)
+
+
 def on_env(env, config, files):
     env.tests["startswith"] = str.startswith
+    env.globals["get_page"] = get_page
+    env.globals["get_notes"] = get_notes
